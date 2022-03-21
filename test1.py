2f188d16368b0582146fb120cad6365de2418d3f
AdamShyly <598455320@qq.com>
2022-03-21 20:16:23 +0800

    9.回文数

---
+++ test1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        src = str(x)
+        for i in range(len(src) // 2):
+            if src[i : i + 1] != src[len(src) - i - 1 : len(src) - i]:
+                return False
+        else:
+            return True
+
+

6c1b918239531c88d25c23a24dce6a449fa404d5
AdamShyly <598455320@qq.com>
2022-03-25 20:47:05 +0800

    350. 两个数组的交集 II

---
+++ intersect.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        res = []
+        for i in range(len(nums2)) :
+            if nums2[i] in nums1 :
+                res.append(nums2[i])
+                nums1.remove(nums2[i])
+        return res
+
+test = Solution()
+res = test.intersect([4,4,9,5], [9,4,9,8,4])
+print(res)
